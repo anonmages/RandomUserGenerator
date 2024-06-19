3c9d056ffc4df7bf38ebf0f1efce8d28ae7815b8
anonmages <163333563+anonmages@users.noreply.github.com>
2024-06-19 11:50:02 +0300

    add new variables

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, Blueprint
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 import os
@@ -9,7 +9,7 @@ load_dotenv()
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'default-secret-key')
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'sqlite:///randomusergenerator.db')
+app.config['DB_URI'] = os.getenv('DATABASE_URI', 'sqlite:///random_user_gen.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
@@ -19,15 +19,13 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, nullable=False)
 
-from flask import Blueprint
+random_user_blueprint = Blueprint('random_user_bp', __name__)
 
-random_user_bp = Blueprint('random_user', __name__)
+@random_user_blueprint.route('/generate', methods=['GET'])
+def generate_random_user():
+    return "Random User Generated"
 
-@random_register_bp.route('/generate_user', methods=['GET'])
-def generate_user():
-    return "Random User"
-
-app.register_blueprint(random_user_bp, url_prefix='/random_user')
+app.register_blueprint(random_user_blueprint, url_prefix='/users')
 
 if __name__ == '__main__':
-    app.run(debug=os.getenv('FLASK_debug', True))
+    app.run(debug=os.getenv('FLASK_DEBUG', True))
