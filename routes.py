fe1bc0229e3d641cbb17eff1b39d1576fa5b88f5
anonmages <163333563+anonmages@users.noreply.github.com>
2024-07-04 16:42:28 +0300

    new function

--- routes.py
+++ routes.py
@@ -1,8 +1,12 @@
 from flask import Flask, render_template, jsonify, request
 import os
 import requests
+from flask_caching import Cache
 
 app = Flask(__name__)
+
+app.config['CACHE_TYPE'] = 'simple'
+cache = Cache(app)
 
 random_user_url = os.getenv('RANDOM_USER_API_URL', 'https://randomuser.me/api/')
 
@@ -15,13 +19,21 @@ def generate_random_user():
     return generate_user_with_nationality()
 
 @app.route('/generate/<nationality>', methods=['GET'])
+@cache.cached(timeout=300, query_string=True)
 def generate_user_with_nationality(nationality=None):
     params = {}
     if nationality:
         params['nat'] = nationality
+    cache_key = 'nationality_{}'.format(nationality if nationality else 'all')
+    cached_result = cache.get(cache_key)
+    if cached_result:
+        return cached_result
+
     response = requests.get(random_user_url, params=params)
     if response.ok:
-        return jsonify(response.json())
+        result = jsonify(response.json())
+        cache.set(cache_key, result, timeout=300)
+        return result
     else:
         return jsonify({"error": "Failed to fetch user data"}), 500
 
